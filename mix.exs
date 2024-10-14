89181f7ab322472a5884cc32a71dc2ef40b13462
chirchir <chirchir7370@gmail.com>
2024-10-14 19:29:30 +0300

    rmq libarary

--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule Airlink.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.2"},
-      {:httpoison, "~> 2.2"}
+      {:httpoison, "~> 2.2"},
+      {:gen_rmq, "~> 4.0"}
     ]
   end
 
