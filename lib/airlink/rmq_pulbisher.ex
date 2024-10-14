59c6e3dbbe925b90868f3e5d5057892d9d02874c
chirchir <chirchir7370@gmail.com>
2024-10-14 19:29:50 +0300

    rmq publisher

---
+++ lib/airlink/rmq_pulbisher.ex
@@ -0,0 +1,48 @@
+defmodule Airlink.RmqPulbisher do
+  @behaviour GenRMQ.Publisher
+  require Logger
+
+  def start_link() do
+    GenRMQ.Publisher.start_link(__MODULE__, name: __MODULE__)
+  end
+
+  @impl true
+  def init() do
+    config = get_options()
+
+    [
+      exchange: config[:exchange],
+      uri: config[:url],
+      durable: true
+    ]
+  end
+
+  def child_spec(_arg) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, []}
+    }
+  end
+
+  def publish(encoded_data, queue) when is_binary(encoded_data) and byte_size(encoded_data) > 0 do
+    case GenRMQ.Publisher.publish(__MODULE__, encoded_data, queue) do
+      :ok ->
+        {:ok, :ok}
+
+      {:ok, :confirmed} ->
+        {:ok, :ok}
+
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  def publish(_message, _queue) do
+    {:error, "Invalid data"}
+  end
+
+  defp get_options() do
+    :diralink
+    |> Application.get_env(__MODULE__)
+  end
+end
