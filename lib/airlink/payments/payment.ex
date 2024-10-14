67962dd02bf1ea48ba0e69bf46404de58c759de4
chirchir <chirchir7370@gmail.com>
2024-10-14 08:18:09 +0300

    payments embeded struct

---
+++ lib/airlink/payments/payment.ex
@@ -0,0 +1,33 @@
+defmodule Airlink.Payments.Payment do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key false
+  embedded_schema do
+    field :customer_id, Ecto.UUID
+    field :company_id, Ecto.UUID
+    field :plan_id, Ecto.UUID
+    field :phone_number, :string
+  end
+
+  def changeset(payment, attrs) do
+    payment
+    |> cast(attrs, [:customer_id, :company_id, :plan_id, :phone_number])
+    |> validate_required([:customer_id, :company_id, :plan_id, :phone_number])
+    |> validate_mobile_number(:phone_number)
+  end
+
+  ## validate mobile number
+  def validate_mobile_number(changeset, field) do
+    validate_change(changeset, field, fn _, value ->
+      case is_valid_mobile_number?(value) do
+        true -> []
+        false -> [{field, "must be exactly 13 digits"}]
+      end
+    end)
+  end
+
+  defp is_valid_mobile_number?(mobile_number) do
+    Regex.match?(~r/^\d{13}$/, mobile_number)
+  end
+end
